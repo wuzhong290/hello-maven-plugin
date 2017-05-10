31ea554758e1477e80ab8fab35a32cde946554e3
wuzhong <419479707@qq.com>
2017-05-10 16:52:32 +0800

    Initial commit

---
+++ src/main/java/sample/plugin/GreetingMojo.java
@@ -0,0 +1,17 @@
+package sample.plugin;
+
+/**
+ * Created by wuzhong on 2017/5/10.
+ */
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.Mojo;
+
+@Mojo( name = "sayhi")
+public class GreetingMojo extends AbstractMojo {
+
+    public void execute() throws MojoExecutionException, MojoFailureException {
+        getLog().info( "Hello, world." );
+    }
+}
