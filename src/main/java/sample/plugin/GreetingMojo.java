c05549b298404625bb39268cde4ed5d14b50f089
wuzhong <419479707@qq.com>
2017-05-10 17:23:15 +0800

    初始化

--- src/main/java/sample/plugin/GreetingMojo.java
+++ src/main/java/sample/plugin/GreetingMojo.java
@@ -3,12 +3,15 @@ package sample.plugin;
 /**
  * Created by wuzhong on 2017/5/10.
  */
+
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
-import org.apache.maven.plugins.annotations.Mojo;
 
-@Mojo( name = "sayhi")
+/**
+ * @author wuzhong
+ * @goal sayhi
+ */
 public class GreetingMojo extends AbstractMojo {
 
     public void execute() throws MojoExecutionException, MojoFailureException {
